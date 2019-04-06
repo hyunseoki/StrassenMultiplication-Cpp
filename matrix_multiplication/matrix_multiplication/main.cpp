d34a4ec1320c4e57444d787d953794b1a99dc227
Your Name <you@example.com>
2019-04-06 18:37:45 +0900

    -. nanoseconds analysis is applied

--- matrix_multiplication/matrix_multiplication/main.cpp
+++ matrix_multiplication/matrix_multiplication/main.cpp
@@ -1,123 +1,106 @@
+#pragma once
+
 #include <iostream>
-#include <time.h>
 #include <opencv2\opencv.hpp>
+#include <time.h>
+#include <chrono>
 
 using std::cout;
-using namespace cv;
-
-template<typename T, size_t N>
-void MatrixMult_Standard(const T A[N][N], const T B[N][N]);
-
-template<typename T, size_t N>
-void MatrixMult_OpenMP(const T A[N][N], const T B[N][N]);
+using cv::Mat;
+using std::chrono::high_resolution_clock;
 
 template<typename T, size_t N>
 void MatrixDisp(const T A[N][N]);
 
+template<typename T, size_t N>
+void MatrixMult_Standard(const T A[N][N], const T B[N][N], T C[N][N]);
+
+template<typename T, size_t N>
+void MatrixMult_OpenMP(const T A[N][N], const T B[N][N], T C[N][N]);
+
 void main()
 {
-	const size_t N = 8;
+	const size_t N = 250;
+	static_assert(N < 257, "Matrix dimension must be less than 257");
 
-	float data1[] = { 
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-	};
-	float data2[] = {
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-	};
+	Mat cv_A(N, N, CV_32F);
+	Mat cv_B(N, N, CV_32F);
 
-	float A[N][N]{
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-	};
+	float A[N][N], B[N][N], C[N][N];
+	
+	srand(time(NULL));
 
-	float B[N][N]{
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f,	1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-		1.2f, 2.3f, 3.2f, 4.5f, 1.2f, 2.3f, 3.2f, 4.5f,
-	};
+	float ranN1, ranN2;
 
-	Mat m1(N, N, CV_32F, data1);
-	Mat m2(N, N, CV_32F, data2);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			ranN1 = (float)(rand())*10/32767; // 0 ~ 10.0 ³­¼ö
+			ranN2 = (float)(rand())*10/32767;
+			cv_A.at<float>(i, j) = ranN1;
+			cv_B.at<float>(i, j) = ranN2;
+			A[i][j] = ranN1;
+			B[i][j] = ranN2;
+		}
+	}
 
 #ifdef _DEBUG
 	cout << "Matrix A: \n";
 	MatrixDisp<float, N>(A);
 	cout << "Matrix B: \n";
 	MatrixDisp<float, N>(B);
+
 	cout << "(opencv) A x B  \n";
+	cout << cv_A* cv_B << "\n\n";
 #endif
 
-#ifdef _DEBUG
-	cout << m1* m2 << "\n\n";
-#endif
+	auto start = high_resolution_clock::now();
+	cv_A * cv_B;
+	auto end = high_resolution_clock::now();
+	cout << "opencv run time = " << (end - start).count()/1000 << "micro sec.\n\n";
 
-	clock_t start1 = clock();
-	m1 * m2;
-	cout << "opencv run time = " << clock() - start1 << "millisec.\n\n";
+	clock_t cstart = clock();
+	cv_A * cv_B;
+	cout << "opencv run time = " << clock() - cstart << "mms.\n\n";
 
-	clock_t start = clock();
-	MatrixMult_Standard<float, N>(A, B);
-	cout << "standard run time = " << clock() - start << "millisec.\n\n";
-
-}
-
-template<typename T, size_t N>
-void MatrixMult_Standard(const T A[N][N], const T B[N][N])
-{
-	static_assert(N > 1, "Matrix dimension must be greater than 1");
-
-	T C[N][N];
-	T dot_product;
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-			dot_product = 0;
-
-			for (int k = 0; k < N; k++)
-			{
-				dot_product += A[i][k] * B[k][j];
-			}
-
-			C[i][j] = dot_product;
-		}
-	}
+	start = high_resolution_clock::now();
+	MatrixMult_Standard<float, N>(A, B, C);
+	end = high_resolution_clock::now();
 
 #ifdef _DEBUG
-	cout << "(Standard) A x B \n";
+	cout << "(Standard) A x B  \n";
 	MatrixDisp<float, N>(C);
 #endif
+
+	cout << "standard run time = " << (end - start).count()/1000 << "micro sec.\n\n";
+
+	cstart = clock();
+	MatrixMult_Standard<float, N>(A, B, C);
+	cout << "standard run time = " << clock() - cstart << "mms.\n\n";
+///////////////////////////////////////////////////////////////////////////////////////////
+
+	start = high_resolution_clock::now();
+	MatrixMult_OpenMP<float, N>(A, B, C);
+	end = high_resolution_clock::now();
+
+#ifdef _DEBUG
+	cout << "(OpenMP) A x B  \n";
+	MatrixDisp<float, N>(C);
+#endif
+
+	cout << "OpenMP run time = " << (end - start).count() / 1000 << "micro sec.\n\n";
+
+	cstart = clock();
+	MatrixMult_OpenMP<float, N>(A, B, C);
+	cout << "OpenMP run time = " << clock() - cstart << "mms.\n\n";
 }
 
 template<typename T, size_t N>
 void MatrixDisp(const T A[N][N])
 {
+	static_assert(N > 1, "Matrix dimension must be greater than 1");
+
 	cout << "[";
 	for (int i = 0; i < N; i++)
 	{
@@ -134,7 +117,48 @@ void MatrixDisp(const T A[N][N])
 }
 
 template<typename T, size_t N>
-void MatrixMult_OpenMP(const T A[N][N], const T B[N][N])
+void MatrixMult_Standard(const T A[N][N], const T B[N][N], T C[N][N])
 {
+	static_assert(N > 1, "Matrix dimension must be greater than 1");
 
+	T dot_product;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			dot_product = 0;
+
+			for (int k = 0; k < N; k++)
+			{
+				dot_product += A[i][k] * B[k][j];
+			}
+
+			C[i][j] = dot_product;
+		}
+	}
+}
+
+template<typename T, size_t N>
+void MatrixMult_OpenMP(const T A[N][N], const T B[N][N], T C[N][N])
+{
+	static_assert(N > 1, "Matrix dimension must be greater than 1");
+
+	T dot_product;
+	
+	#pragma omp parallel for
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			dot_product = 0;
+
+			for (int k = 0; k < N; k++)
+			{
+				dot_product += A[i][k] * B[k][j];
+			}
+
+			C[i][j] = dot_product;
+		}
+	}
 }
